35c38c478294849f183c96a27a33e6536d21589a
LauraPlaza <0619967435@uma.es>
2022-12-16 09:06:49 +0100

    ADAA

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HHOLAAAAPROBANDO
+          Edit <code>src/App.js</code>  CAMBIANDO ESA MIERDA
         </p>
         <a
           className="App-link"
